45bfbc68ebb22a627b394410fa929923723f627b
hyx131 <selin.hyx@gmail.com>
2021-06-01 14:14:28 -0400

    lumen-tree/init - project init with storybook

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Tree = () => {
+  return (
+    <p>Hello</p>
+  )
+}
+
+export default Tree
---
+++ stories/tree.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tree from '../src'
+
+
+export default {
+  title: 'Tree',
+  component: Tree,
+}
+
+export const Normal = () => {
+  return <Tree />
+}
